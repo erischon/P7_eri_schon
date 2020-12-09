f63ef3b5fd9f15e80b5f24a42f53c54e2857e035
Eri Schon <erischon@gmail.com>
2020-12-09 15:37:25 +0100

    Update script.js

--- grandpy/static/script.js
+++ grandpy/static/script.js
@@ -1,3 +1,12 @@
-// const display = document.querySelector("#sec-search")
+const button = document.querySelector('.btn')
 
-// console.log(display.setAttribute("class", "disp-none"))
+button.addEventListener('click', () => myFunction())
+
+function myFunction() {
+    var x = document.getElementById("answerElement");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
