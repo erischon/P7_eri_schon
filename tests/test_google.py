4e6b0554276a1416184cb5c218ebd2db554344c0
Eri Schon <erischon@gmail.com>
2020-12-16 10:09:58 +0100

    Update test_google.py

--- tests/test_google.py
+++ tests/test_google.py
@@ -1,9 +1,9 @@
-def test_google_api_connection():
-    result = f()
-    assert result == "ce que j'attends"
+# def test_google_api_connection():
+#     result = f()
+#     assert result == "ce que j'attends"
 
-def test_answer_to_resquest():
-    pass
+# def test_answer_to_resquest():
+#     pass
 
-def test_format_of_answer():
-    pass
+# def test_format_of_answer():
+#     pass
