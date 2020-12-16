c5738172a5d8b4b1b6c8e2d5d9ab2ceaa301553f
Eri Schon <erischon@gmail.com>
2020-12-16 09:33:49 +0100

    Create test_parser.py

---
+++ tests/test_parser.py
@@ -0,0 +1,15 @@
+# lowercase
+def test_to_lowercase():
+    pass
+
+# punctuation
+def test_remove_punctuations():
+    pass
+
+# accents
+def test_remove_accents():
+    pass
+
+# stopwords
+def test_remove_stopwords():
+    pass
