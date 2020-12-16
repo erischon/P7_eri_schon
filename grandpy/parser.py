6e9087154c70258e7719ba539e6a56694be58ad3
Eri Schon <erischon@gmail.com>
2020-12-16 10:49:25 +0100

    Update parser.py

--- grandpy/parser.py
+++ grandpy/parser.py
@@ -1,4 +1,4 @@
-
+import re
 
 class Parser:
     """ """
@@ -10,8 +10,16 @@ class Parser:
         """ """
         return value.lower()
 
+    def punctuation(self, value):
+        value = re.sub(r'_', '', value)
+        value = re.sub(r'\'', ' ', value)
+        value = re.sub(r'[^\w\s]','', value)
+
+        return value
+
 if __name__ == "__main__":
     parser = Parser()
     
     # === Tests of methods ===
     # print(parser.lowercase("ToTO"))
+    print(parser.punctuation("hop,hop, hop, zut: ça v;a plus! ..../_ super..."))
