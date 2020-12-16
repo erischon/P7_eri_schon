b9b6aa6a8f2a33ff0736e6b56a4128e931a5b853
Eri Schon <erischon@gmail.com>
2020-12-16 10:10:00 +0100

    Update test_parser.py

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,15 +1,31 @@
+from grandpy.parser import Parser
+
+parser = Parser()
+
 # lowercase
 def test_to_lowercase():
-    pass
+    result_a = parser.lowercase("jklMnOpQrsTu")
+    result_b = parser.lowercase("Monsieur et Madame TrucMuche")
+    assert result_a == "jklmnopqrstu"
+    assert result_b == "monsieur et madame trucmuche"
 
 # punctuation
 def test_remove_punctuations():
-    pass
+    result_a = parser.punctuation("hop,hop, hop, zut: ça v;a plus! ..../_ super...")
+    result_b = parser.punctuation("(c'est... top) #& par,ce === que j'ai-- \{réussi] à fa}ire( nimp)")
+    result_c = parser.punctuation(";.,:!?=()[]{}\/")
+    assert result_a == "hop hop hop zut ça va plus super"
+    assert result_b == "c'est top parce que j'ai réussi à faire nimp"
+    assert result_c == ""
 
-# accents
-def test_remove_accents():
-    pass
+# # accents
+# def test_remove_accents():
+#     pass
 
-# stopwords
-def test_remove_stopwords():
-    pass
+# # stopwords
+# def test_remove_stopwords():
+#     pass
+
+# # class test
+# def clean_text():
+#     pass
