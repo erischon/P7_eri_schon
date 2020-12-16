027d788301032efe9e425ec0d5c470cd9d6f20df
Eri Schon <erischon@gmail.com>
2020-12-16 10:09:53 +0100

    Create parser.py

---
+++ grandpy/parser.py
@@ -0,0 +1,17 @@
+
+
+class Parser:
+    """ """
+
+    def __init__(self):
+        pass
+
+    def lowercase(self, value):
+        """ """
+        return value.lower()
+
+if __name__ == "__main__":
+    parser = Parser()
+    
+    # === Tests of methods ===
+    # print(parser.lowercase("ToTO"))
