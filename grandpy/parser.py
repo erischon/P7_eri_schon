42259546f44c3ea0bd0e522f32c78a4eedd38214
Eri Schon <erischon@gmail.com>
2020-12-16 13:50:47 +0100

    Update parser.py

--- grandpy/parser.py
+++ grandpy/parser.py
@@ -12,11 +12,12 @@ class Parser:
 
     def punctuation(self, value):
         """ I remove the punctuations. """
-        value = re.sub(r'_', '', value)
-        value = re.sub(r',', ' ', value)
-        value = re.sub(r'\'', ' ', value)
+        value = re.sub('_', ' ', value)
+        value = re.sub(',', ' ', value)
+        value = re.sub('\'', ' ', value)
         value = re.sub(r'[^\w\s]','', value)
-        value = re.sub('\s+',' ',value)
+        # value = re.sub(r'[^a-zA-Z\d\s]','',value)
+        value = re.sub(r'\s+',' ',value)
         return value
 
 if __name__ == "__main__":
@@ -24,4 +25,4 @@ if __name__ == "__main__":
     
     # === Tests of methods ===
     # print(parser.lowercase("ToTO"))
-    # print(parser.punctuation("hop,hop, hop, zut: ça v;a plus! ..../_ super..."))
+    print(parser.punctuation("hop,hop, hop, zut: ça v;a plus! ..../_ super..."))
