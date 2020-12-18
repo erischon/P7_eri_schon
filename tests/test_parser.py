96f4ad007a3cfa6779229f03ea32708ff096b311
Eri Schon <erischon@gmail.com>
2020-12-18 09:14:17 +0100

    Update test_parser.py

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,6 +4,7 @@ parser = Parser()
 
 # lowercase
 def test_to_lowercase():
+    """ """
     result_a = parser.lowercase("jklMnOpQrsTu")
     result_b = parser.lowercase("Monsieur et Madame TrucMuche")
     assert result_a == "jklmnopqrstu"
@@ -11,12 +12,21 @@ def test_to_lowercase():
 
 # punctuation
 def test_remove_punctuations():
+    """ I remove all the punctuation. """
     result_a = parser.punctuation("hop,hop, hop, zut: ça v;a plus! ..../_ super...")
     result_b = parser.punctuation("(c'est... top) #& par-ce === que j'ai-- \{réussi] à fa}ire( nimp)")
     result_c = parser.punctuation(";.,:!?=()[]{}\/")
     assert result_a == "hop hop hop zut ça va plus super"
     assert result_b == "c est top parce que j ai réussi à faire nimp"
     assert result_c == " "
+
+# tokenization + Stemming
+def test_tokenization():
+    """ """
+    result_a = parser.tokenization("hop hop hop zut ça va plus super")
+    result_b = parser.tokenization("c est top parce que j ai réussi à faire nimp")
+    assert result_a == "['hop', 'hop', 'hop', 'zut', 'ça', 'va', 'plus', 'super']"
+    assert result_b == "['est', 'top', 'parce', 'que', 'ai', 'réussi', 'faire', 'nimp']"
 
 # # spacing
 # def test_remove_spacing():
