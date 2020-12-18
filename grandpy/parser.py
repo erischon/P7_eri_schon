77f2a68f20645ad7c8970fa4b5bf6d47733d319b
Eri Schon <erischon@gmail.com>
2020-12-18 09:14:15 +0100

    Update parser.py

--- grandpy/parser.py
+++ grandpy/parser.py
@@ -1,10 +1,12 @@
 import re
+import nltk
+from nltk.stem.snowball import FrenchStemmer
 
 class Parser:
     """ """
 
     def __init__(self):
-        pass
+        self.stemmer = FrenchStemmer()
 
     def lowercase(self, value):
         """ I lowercase the text. """
@@ -16,13 +18,20 @@ class Parser:
         value = re.sub(',', ' ', value)
         value = re.sub('\'', ' ', value)
         value = re.sub(r'[^\w\s]','', value)
-        # value = re.sub(r'[^a-zA-Z\d\s]','',value)
         value = re.sub(r'\s+',' ',value)
         return value
+
+    def tokenization(self, value):
+        """ """
+        value = nltk.word_tokenize(value)
+        return value
+
+
 
 if __name__ == "__main__":
     parser = Parser()
     
     # === Tests of methods ===
     # print(parser.lowercase("ToTO"))
-    print(parser.punctuation("hop,hop, hop, zut: ça v;a plus! ..../_ super..."))
+    # print(parser.punctuation("hop,hop, hop, zut: ça v;a plus! ..../_ super..."))
+    print(parser.tokenization("hop hop hop zut ça va plus super"))
