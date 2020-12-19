1bdfd3b7fa304cd12a2e16b68f78def356537dbc
Eri Schon <erischon@gmail.com>
2020-12-19 20:58:48 +0100

    Update test_parser.py

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -4,7 +4,7 @@ parser = Parser()
 
 # lowercase
 def test_to_lowercase():
-    """ """
+    """ I test the lowercase. """
     result_a = parser.lowercase("jklMnOpQrsTu")
     result_b = parser.lowercase("Monsieur et Madame TrucMuche")
     assert result_a == "jklmnopqrstu"
@@ -12,7 +12,7 @@ def test_to_lowercase():
 
 # punctuation
 def test_remove_punctuations():
-    """ I remove all the punctuation. """
+    """ I test the remove of all the punctuation. """
     result_a = parser.punctuation("hop,hop, hop, zut: ça v;a plus! ..../_ super...")
     result_b = parser.punctuation("(c'est... top) #& par-ce === que j'ai-- \{réussi] à fa}ire( nimp)")
     result_c = parser.punctuation(";.,:!?=()[]{}\/")
@@ -22,7 +22,7 @@ def test_remove_punctuations():
 
 # tokenization
 def test_tokenization():
-    """ """
+    """ I test the tokenization. """
     result_a = parser.tokenization("hop hop hop zut ça va plus super")
     result_b = parser.tokenization("c est top parce que j ai réussi à faire nimp")
     assert result_a == ['hop', 'hop', 'hop', 'zut', 'ça', 'va', 'plus', 'super']
@@ -30,23 +30,13 @@ def test_tokenization():
 
 #stop words
 def test_remove_stop_words():
-    """ """
+    """ I test the remove of french stop words.  """
     result_a = parser.remove_stopwords(['vives', 'toujours', 'puisque', 'orléans', 'pure', '16', 'camille'])
     assert result_a == ['orléans', '16', 'camille']
 
 # class test
 def test_parser():
+    """ I test the parser. """
     result_a = parser.parser("je cherche l'adresse de la maison de la radio à paris ou à marseille")
     # assert result_a == ['cherche', 'adresse', 'maison', 'radio', 'paris', 'marseille']
     assert result_a == "cherche adresse maison radio paris marseille"
-
-
-
-
-# # spacing
-# def test_remove_spacing():
-#     pass
-
-# # accents
-# def test_remove_accents():
-#     pass
