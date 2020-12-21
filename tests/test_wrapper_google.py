b0049c468be7df1c3e730734dadc4f8ae16d52ce
Eri Schon <erischon@gmail.com>
2020-12-21 10:02:07 +0100

    Update test_wrapper_google.py

--- tests/test_wrapper_google.py
+++ tests/test_wrapper_google.py
@@ -1,6 +1,10 @@
+from grandpy.wrapper_google import WrapperGoogle
+
+wgoogle = WrapperGoogle()
+
 def test_google_api_connection():
-    result = f()
-    assert result == "ce que j'attends"
+    result_a = wgoogle.connection()
+    assert result_a == "je veux une fonction"
 
 # def test_answer_to_resquest():
 #     pass
