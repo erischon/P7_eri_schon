85eded33384e16c11ad06a634bda522ae530874c
Eri Schon <erischon@gmail.com>
2020-12-21 11:33:40 +0100

    Update test_wrapper_google.py

--- tests/test_wrapper_google.py
+++ tests/test_wrapper_google.py
@@ -1,13 +1,20 @@
 from grandpy.wrapper_google import WrapperGoogle
 
 wgoogle = WrapperGoogle()
+result = wgoogle.request()
 
 def test_google_request():
     result_a = str(type(wgoogle.request()))
+    assert result_a == "<class 'list'>"
+
+def test_number_of_results():
+    result_a = str(type(wgoogle.number_of_results(result)))
+    assert result_a == "<class 'int'>"
+
+def test_coordinates():
+    result_a = str(type(wgoogle.coordinates(result)))
     assert result_a == "<class 'dict'>"
 
-# def test_answer_to_resquest():
-#     pass
-
-# def test_format_of_answer():
-#     pass
+def test_informations():
+    result_a = str(type(wgoogle.informations(result)))
+    assert result_a == "<class 'dict'>"
