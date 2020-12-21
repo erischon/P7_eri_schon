a64c5a4a0247785cbacfeff1c1ec3bc025cd2067
Eri Schon <erischon@gmail.com>
2020-12-21 11:33:37 +0100

    Update wrapper_google.py

--- grandpy/wrapper_google.py
+++ grandpy/wrapper_google.py
@@ -1,5 +1,5 @@
 import requests
-from config import GURL, GKEY, UAGENT
+from .config import GURL, GKEY, UAGENT
 
 
 class WrapperGoogle:
@@ -56,5 +56,5 @@ if __name__ == "__main__":
 
     # print(wgoogle.request(), type(wgoogle.request()))
     # print(wgoogle.number_of_results(wgoogle.request()))
-    print(wgoogle.coordinates(wgoogle.request()))
-    print(wgoogle.informations(wgoogle.request()))
+    # print(wgoogle.coordinates(wgoogle.request()))
+    # print(wgoogle.informations(wgoogle.request()))
