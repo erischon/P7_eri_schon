7b38c3964a697f7c88e6f8795e1465e74934ebf1
Eri Schon <erischon@gmail.com>
2020-12-21 11:20:27 +0100

    Update wrapper_google.py

--- grandpy/wrapper_google.py
+++ grandpy/wrapper_google.py
@@ -13,7 +13,7 @@ class WrapperGoogle:
             "key": GKEY
         }
 
-    def request(self, query="paris"):
+    def request(self, query="cherche adresse maison radio paris marseille"):
         try:
             request = requests.get(
                 url = self.URL, 
@@ -24,19 +24,37 @@ class WrapperGoogle:
                 headers = self.HEADERS
             )
             results = request.json()
+            results = results["results"]
             return results
 
         except requests.RequestException as exception:
             print(exception)
 
+    def number_of_results(self, results):
+        """ """
+        return len(results)
+
     def coordinates(self, results):
         """ """
-        request = self.connection(params={"query": query})
-        print(request)
+        result = {
+            "geometry": results[0].get('geometry')
+        }
+        return result
+
+    def informations(self, results):
+        """ """
+        result = {
+            "name": results[0].get('name'),
+            "formatted_address": results[0].get('formatted_address'),
+            "types": results[0].get('types')
+        }
+        return result
 
 
 if __name__ == "__main__":
     wgoogle = WrapperGoogle()
 
-    print(wgoogle.request(), type(wgoogle.request()))
-    # print(wgoogle.request("paris"))
+    # print(wgoogle.request(), type(wgoogle.request()))
+    # print(wgoogle.number_of_results(wgoogle.request()))
+    print(wgoogle.coordinates(wgoogle.request()))
+    print(wgoogle.informations(wgoogle.request()))
