8b6566158f203fc5fbf80cb8999a63140fee24e8
Eri Schon <erischon@gmail.com>
2020-12-21 10:02:05 +0100

    Update wrapper_google.py

--- grandpy/wrapper_google.py
+++ grandpy/wrapper_google.py
@@ -1,24 +1,33 @@
 import requests
-from config import *
+from config import GURL, GKEY, UAGENT
 
 
 class WrapperGoogle:
     """ """
 
     def __init__(self):
-        self.url = "https://maps.googleapis.com/maps/api/geocode/json"
-        self.params = {
+        self.URL = GURL
+        self.HEADERS = {"User-Agent": UAGENT}
+        self.PARAMS = {
             "key": GKEY
         }
 
     def connection(self):
         try:
-            print(self.params["key"])
+            connection = requests.get(
+                url=self.URL, 
+                params=self.PARAMS, 
+                headers=self.HEADERS
+            )
+            return connection
 
-        except:
-            pass
+        except requests.RequestException as exception:
+            print(exception)
+
+    def request(self, query):
+        pass
 
 if __name__ == "__main__":
     wgoogle = WrapperGoogle()
 
-    print(wgoogle.connection())
+    print(wgoogle.connection(), type(wgoogle.connection()))
