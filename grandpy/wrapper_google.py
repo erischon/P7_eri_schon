f32f75bd5ecb0a6f80e681460ddfc8a4fec00d6f
Eri Schon <erischon@gmail.com>
2020-12-21 10:26:31 +0100

    Update wrapper_google.py

--- grandpy/wrapper_google.py
+++ grandpy/wrapper_google.py
@@ -8,26 +8,35 @@ class WrapperGoogle:
     def __init__(self):
         self.URL = GURL
         self.HEADERS = {"User-Agent": UAGENT}
+        self.GKEY = GKEY
         self.PARAMS = {
             "key": GKEY
         }
 
-    def connection(self):
+    def connection(self, query="paris"):
         try:
-            connection = requests.get(
-                url=self.URL, 
-                params=self.PARAMS, 
-                headers=self.HEADERS
+            request = requests.get(
+                url = self.URL, 
+                params = {
+                    "key": GKEY,
+                    "query": query
+                }, 
+                headers = self.HEADERS
             )
-            return connection
+            results = request.json()
+            return results
 
         except requests.RequestException as exception:
             print(exception)
 
     def request(self, query):
-        pass
+        """ """
+        request = self.connection(params={"query": query})
+        print(request)
+
 
 if __name__ == "__main__":
     wgoogle = WrapperGoogle()
 
     print(wgoogle.connection(), type(wgoogle.connection()))
+    # print(wgoogle.request("paris"))
