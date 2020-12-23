530d7240a2f3f49c250c0010db2243df76b41d25
Eri Schon <erischon@gmail.com>
2020-12-23 12:58:33 +0100

    Update main.py

--- grandpy/main.py
+++ grandpy/main.py
@@ -16,14 +16,22 @@ class GrandPy:
         """ """
         result = input("un texte : ")
         text_result = self.gpparser.parser(result)
+        print("Le texte parsé : ", text_result)
         google_result = self.wgoogle.request(text_result)
+        print("Le résultat de la recherche Google : ", google_result)
+        if google_result == None:
+            print("Je ne trouve rien !")
+            return
         place_result = self.wgoogle.informations(google_result)
-        print(place_result)
+        print("Les informations sur le lieu : ", place_result)
         location_result = self.wgoogle.coordinates(google_result)
+        print("La location Google : ", location_result)
         coord_result = self.wwiki.location_to_coord(location_result)
+        print("coordonnées à passer à Wikipedia : ", coord_result)
         pageid_result = self.wwiki.coord_to_pageid(coord_result)
-        text_result = self.wwiki.wiki_text(pageid_result)
-        print(text_result)
+        print("L'ID de la page wikipedia : ", pageid_result)
+        text_result = self.wwiki.wiki_text(str(pageid_result))
+        print("Le texte : ", text_result)
 
 
 
