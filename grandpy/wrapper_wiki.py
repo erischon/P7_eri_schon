4f809dd1ffdb02ab8f16526988c1068fde50c1ee
Eri Schon <erischon@gmail.com>
2020-12-24 11:15:14 +0100

    Update wrapper_wiki.py

--- grandpy/wrapper_wiki.py
+++ grandpy/wrapper_wiki.py
@@ -14,6 +14,30 @@ class WrapperWiki:
         """ """
         return f"{location.get('location').get('lat')}|{location.get('location').get('lng')}"
 
+    # def name_to_pageid(self, name):
+    #     """ """
+    #     try:
+    #         request = requests.get(
+    #             url = self.URL, 
+    #             params = {
+    #                 "action": "query",
+    #                 "list": "search",
+    #                 "srsearch": name,
+    #                 "format": "json",
+    #             },
+    #         )
+    #         results = request.json()
+
+    #         if len(results) < 1:
+    #             print("Désolé, je n'ai rien à dire")
+    #         else:
+    #             result = results.get('query').get('geosearch')
+    #             result = result[0].get('pageid')
+    #             return result
+
+    #     except requests.RequestException as exception:
+    #         print(exception)
+
     def coord_to_pageid(self, coord="48.76569917989272|2.392394129892722"):
         """ """
         try:
@@ -23,7 +47,7 @@ class WrapperWiki:
                     "action": "query",
                     "list": "geosearch",
                     "gscoord": coord,
-                    "gsradius": 1000,
+                    "gsradius": 100,
                     "gslimit": 1,
                     "format": "json",
                 },
@@ -74,3 +98,4 @@ if __name__ == "__main__":
     # print(wwiki.location_to_coord({'location': {'lat': 48.76569917989272, 'lng': 2.392394129892722}}), type(wwiki.location_to_coord({'location': {'lat': 48.76569917989272, 'lng': 2.392394129892722}})))
     # print(wwiki.coord_to_pageid("48.76569917989272|2.392394129892722"), type(wwiki.coord_to_pageid("48.76569917989272|2.392394129892722")))
     # print(wwiki.wiki_text("5548980"))
+    # print(wwiki.name_to_pageid("mairie_de_thiais"))
