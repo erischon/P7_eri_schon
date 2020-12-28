dff5bacc762f4ad4a800adec86c69228401a7447
Eri Schon <erischon@gmail.com>
2020-12-28 11:26:33 +0100

    Update views.py

--- grandpy/views.py
+++ grandpy/views.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify
+from main import GrandPy
+from gp_parser import GPParser
 
 app = Flask(__name__)
 
@@ -9,7 +11,12 @@ def index():
 @app.route('/livesearch', methods=['POST', 'GET'])
 def livesearch():
     searchbox = request.form.get("text")
-    return jsonify(searchbox)
+    parser = GPParser()
+    result = parser.parser(searchbox)
+    # grandpy = GrandPy()
+    # result = grandpy.main_coord(searchbox)
+
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
