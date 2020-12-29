b3263881fadbe06b90ad8ef605f662b4e2b39311
Eri Schon <erischon@gmail.com>
2020-12-29 11:07:37 +0100

    Update script.js

--- grandpy/static/script.js
+++ grandpy/static/script.js
@@ -2,7 +2,6 @@
 $(document).ready(function(){
             
     $("#form").submit(function(e){
-        event.preventDefault(); //prevent default action
 
         textinlivebox = $("#livebox").val();
         
@@ -11,12 +10,16 @@ $(document).ready(function(){
             url:"/livesearch",
             data:{text:textinlivebox},
             success: function(res){
-                $("#result_query").html(res["query"]);
-                $("#result_name").html(res["ginfos"]["name"]);
-                $("#result_adress").html(res["ginfos"]["formatted_address"]);
-                $("#result_wiki").html(res["wtext"]);
+               response(res);
             }
         })
     });
 
 })
+
+function response(res) {
+    $("#result_query").html(res["query"]);
+    $("#result_name").html(res["ginfos"]["name"]);
+    $("#result_adress").html(res["ginfos"]["formatted_address"]);
+    $("#result_wiki").html(res["wtext"]);
+}
