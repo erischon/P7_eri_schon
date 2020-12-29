6803f2fb5b33d309892aad09139e1366972d9022
Eri Schon <erischon@gmail.com>
2020-12-29 08:52:20 +0100

    Update main.py

--- grandpy/main.py
+++ grandpy/main.py
@@ -14,19 +14,32 @@ class GrandPy:
 
     def main_coord(self, query):
         """ """
-        text_result = self.gpparser.parser(query)
-        print("\n== Le texte parsé : ", text_result, "==")
-        google_result = self.wgoogle.request(text_result)
-        google_nbe_result = self.wgoogle.number_of_results(google_result)
-        print("\n== Le nombre de résultat de la recherche Google : ", google_nbe_result, "==")
-        print("\n== Le résultat de la recherche Google : ", google_result, "==")
+        response = {}
+        response["query"] = query
+
+        # Parsing
+        parsing_result = self.gpparser.parser(query)
+        if parsing_result == "wiki":
+            response["flow"] = False
+            return response
+        response["parsing"] = parsing_result
+
+        # Google Search
+        google_result = self.wgoogle.request(parsing_result)
+        # google_nbe_result = self.wgoogle.number_of_results(google_result)
+        # print("\n== Le nombre de résultat de la recherche Google : ", google_nbe_result, "==")
         if google_result == None:
-            print("\n== Désolé, je ne trouve rien avec cette demande ! ==\n")
-            return
+            response["flow"] = False
+            return response
         place_result = self.wgoogle.informations(google_result)
         print("\n== Les informations sur le lieu : ", place_result, "==")
         location_result = self.wgoogle.coordinates(google_result)
         print("\n== La location Google : ", location_result, "==")
+        
+        
+        
+        
+        
         coord_result = self.wwiki.location_to_coord(location_result)
         print("\n== coordonnées à passer à Wikipedia : ", coord_result, "==")
         pageid_result = self.wwiki.coord_to_pageid(coord_result)
@@ -36,10 +49,12 @@ class GrandPy:
         print("\n== L'ID de la page wikipedia : ", pageid_result, "==")
         text_result = self.wwiki.wiki_text(str(pageid_result))
         print("\n== Le texte : ", text_result, "==")
+        return response
 
 
 if __name__ == "__main__":
     grandpy = GrandPy()
 
-    grandpy.main_coord('test')
+    # print(grandpy.main_coord('thiais mairie'))
+    print(grandpy.main_coord('zapzapzapzap'))
     # grandpy.main_name()
