09c8eaa8c82c083d5a3356a9f2f235c81ffd58ff
Eri Schon <erischon@gmail.com>
2020-12-31 10:05:41 +0100

    Update wrapper_wiki.py

--- grandpy/wrapper_wiki.py
+++ grandpy/wrapper_wiki.py
@@ -4,7 +4,7 @@ from .config import Config
 
 
 class WrapperWiki:
-    """ """
+    """ I'm the Wiki Wrapper. """
 
     def __init__(self):
         """ """
@@ -12,11 +12,20 @@ class WrapperWiki:
         self.URL = config.WURL
 
     def location_to_coord(self, location):
-        """ """
+        """ I format the location coordinates
+        In: the Google location coordinates (dict)
+        Act: I format
+        Out: latitude and longitude (str)
+        Out Ex: '48.76569917989272|2.392394129892722'
+        """
         return f"{location.get('location').get('lat')}|{location.get('location').get('lng')}"
 
     def coord_to_pageid(self, coord):
-        """ """
+        """ 
+        In: the formated coordinates (str)
+        Act: I request the Wiki API for a pageid
+        Out: the pageid (int)
+        """
         try:
             request = requests.get(
                 url = self.URL, 
@@ -44,14 +53,18 @@ class WrapperWiki:
             print(exception)
 
     def wiki_text(self, pageid="1509079"):
-        """ """
+        """ 
+        In: a pageid (int)
+        Act: I request the Wiki API for title and text
+        Out: the title of wiki page, and the first sentences.
+        """
         try:
             request = requests.get(
                 url = self.URL, 
                 params = {
                     "action": "query",
                     "prop": "extracts",
-                    "exsentences": 3,
+                    "exsentences": 1,
                     "exlimit": 1,
                     "pageids": pageid,
                     "explaintext": 1,
@@ -60,7 +73,6 @@ class WrapperWiki:
                 },
             )
             results = request.json()
-            # print(results)
 
             if len(results) < 1:
                 print("Désolé, je n'ai rien à dire")
@@ -77,6 +89,7 @@ class WrapperWiki:
 if __name__ == "__main__":
     wwiki = WrapperWiki()
 
+    # === Tests of methods ===
     # print(wwiki.location_to_coord({'location': {'lat': 48.76569917989272, 'lng': 2.392394129892722}}), type(wwiki.location_to_coord({'location': {'lat': 48.76569917989272, 'lng': 2.392394129892722}})))
     # print(wwiki.coord_to_pageid("48.76569917989272|2.392394129892722"), type(wwiki.coord_to_pageid("48.76569917989272|2.392394129892722")))
     # print(wwiki.coord_to_pageid("46.2425288|6.067658499999999"))
