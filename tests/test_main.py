abf24520c19c145df48a32614952f0e04e55311c
Eri Schon <erischon@gmail.com>
2020-12-31 10:32:15 +0100

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,6 @@ gp = GrandPy()
 result = 'mairie de thiais'
 
 def test_main():
-    """ """
+    """ I test the creation of a result. """
     result_a = str(type(gp.main(result)))
     assert result_a == "<class 'dict'>"
